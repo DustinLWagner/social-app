2d3276f3c80e5cd6282f5e5db45c25b42d3c749a
DustinLWagner <DustinWagner@gmail.com>
2025-05-27 23:12:16 -0500

    feat: display only user posts on profile page Add getUserFeed function to feedController to fetch posts by user Update post routes to include /api/posts/users/:id/posts Modify loadFeed to fetch user or global feed dynamically

--- social-app/frontend/public/modules/loadFeed.js
+++ social-app/frontend/public/modules/loadFeed.js
@@ -1,17 +1,17 @@
 import { createPostCard } from "/modules/createPostCard.js";
 
-async function loadFeed() {
+async function loadFeed(userId = null) {
     const feedContainer = document.getElementById('feedContainer');
     const emptyFeedMsg = document.getElementById('emptyFeedMsg');
     const feedFailMsg = document.getElementById('feedFailMsg');
-
     emptyFeedMsg.hidden = true;
     feedFailMsg.hidden = true;
     feedContainer.innerHTML = '';
 
     try {
         //GET posts from /api/posts/feed
-        const response = await fetch('/api/posts/feed', {
+        const postsUrl = userId ? `/api/posts/users/${userId}/posts` : '/api/posts/feed'
+        const response = await fetch(postsUrl, {
             method: 'GET',
         });
         const posts = await response.json();
