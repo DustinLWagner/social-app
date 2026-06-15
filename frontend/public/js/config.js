e170c78ed72934713dc3bb7354e93d4145de5df2
DustinLWagner <DustinWagner@gmail.com>
2026-06-15 01:17:21 -0500

    Fix dynamic URL issue

--- frontend/public/js/config.js
+++ frontend/public/js/config.js
@@ -1,3 +1,3 @@
 // dynamic IP address of the backend server
-const API_BASE = '${window.location.protocol}//${window.location.hostname}:3000/api';
+const API_BASE = `${window.location.protocol}//${window.location.hostname}:3000/api`;
 export default API_BASE;
